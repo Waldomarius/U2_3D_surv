ea103d11077a95b0b51fa3627773b601de6579ef
Waldomarius <40439764+Waldomarius@users.noreply.github.com>
2026-02-02 22:12:46 +0300

    2026.02.02 Добавил тестовую локацию, передвижение/обзор игрока игрока, прыжки игрока.

---
+++ Assets/Scripts/GroundChecker.cs
@@ -0,0 +1,29 @@
+using System;
+using UnityEngine;
+
+public class GroundChecker : MonoBehaviour
+{
+    public Action<bool> OnGroundStateChange;
+    private bool isGrounded;
+    
+    [SerializeField] private LayerMask _groundLayer;
+    [SerializeField] private float _raycastDistance = 0.6f;
+
+    private void Update()
+    {
+        CheckGround();
+    }
+
+    private void CheckGround()
+    {
+        bool hit = Physics.Raycast(transform.position, Vector3.down, _raycastDistance, _groundLayer);
+        if (hit != isGrounded)
+        {
+            isGrounded = hit;
+            // Оповестим всех слушателей об изменении состояния земли
+            OnGroundStateChange?.Invoke(isGrounded);
+        }
+        
+        Debug.DrawRay(transform.position, Vector3.down * _raycastDistance, isGrounded ? Color.green : Color.red);
+    }
+}
